cd730df8c9ab8c5d9e48c10fb55b123de228f5f1
Marco Carraro <carra.rules@gmail.com>
2023-08-11 12:56:15 +0200

    Running commands in Playground

--- Commands.playground/Contents.swift
+++ Commands.playground/Contents.swift
@@ -1,3 +1,35 @@
+//Import Cocoa to access Process (former NSTask)
 import Cocoa
 
-var greeting = "Hello, playground"
+//Create a new Process
+ let process = Process()
+
+//Set the executable URL for the process to a file URL path discovered
+// To discover the command path run "which 'COMMAND'" in Terminal (e.g. "which whoami")
+process.executableURL = URL(filePath: "/sbin/ping")
+
+//Provide the arguments to the process
+// To discover the arguments list run "'COMMAND' man" in Terminal (e.g. "man whoami")
+
+process.arguments = ["-c", "5", "apple.com"]
+
+//Create a picpe to enable communications between process
+let outPipe = Pipe()
+
+//Setup a File Handle to read the data coming through the pipe
+let outFile = outPipe.fileHandleForReading
+
+//Assign the pipe as the standard output for the process
+process.standardOutput = outPipe
+
+do{
+    try process.run()
+    process.waitUntilExit()
+    
+    if let data = try outFile.readToEnd(),
+       let returnValue = String(data: data, encoding: .utf8) {
+        print("Result: \(returnValue)")
+    }
+} catch{
+    print(error)
+}
